5d8a589c3a389f01d871b1b41ccb3553414243c5
Lauri Wiljami Ahonen <lwahonen@iki.fi>
2019-07-31 17:20:19 +0300

    Merge

--- wasync/src/main/java/org/atmosphere/wasync/impl/ClientUtil.java
+++ wasync/src/main/java/org/atmosphere/wasync/impl/ClientUtil.java
@@ -1,25 +1,25 @@
-/*
- * Copyright 2017 Async-IO.org
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-package org.atmosphere.wasync.impl;
+        /*
+         * Copyright 2017 Async-IO.org
+         *
+         * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+         * use this file except in compliance with the License. You may obtain a copy of
+         * the License at
+         *
+         * http://www.apache.org/licenses/LICENSE-2.0
+         *
+         * Unless required by applicable law or agreed to in writing, software
+         * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+         * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+         * License for the specific language governing permissions and limitations under
+         * the License.
+         */
+        package org.atmosphere.wasync.impl;
 
-import org.asynchttpclient.AsyncHttpClient;
-import org.asynchttpclient.DefaultAsyncHttpClient;
-import org.asynchttpclient.DefaultAsyncHttpClientConfig;
-import org.atmosphere.wasync.Options;
-import org.atmosphere.wasync.Socket;
+        import org.asynchttpclient.AsyncHttpClient;
+        import org.asynchttpclient.DefaultAsyncHttpClient;
+        import org.asynchttpclient.DefaultAsyncHttpClientConfig;
+        import org.atmosphere.wasync.Options;
+        import org.atmosphere.wasync.Socket;
 
 /**
  * Util class for building {@link AsyncHttpClient}
@@ -30,38 +30,18 @@ public class ClientUtil {
     private static final String WASYNC_USER_AGENT = "wAsync/2.0";
 
     public final static AsyncHttpClient createDefaultAsyncHttpClient(Options o) {
-<<<<<<< c5c6c287d770ee7f5f40e4e8561956213ed539d9
-		return createDefaultAsyncHttpClient(o.requestTimeoutInSeconds());
-	}
+        return createDefaultAsyncHttpClient(o.requestTimeoutInSeconds());
+    }
 
-	public final static AsyncHttpClient createDefaultAsyncHttpClient(int requestTimeoutInSeconds) {
-		DefaultAsyncHttpClientConfig.Builder b = new DefaultAsyncHttpClientConfig.Builder();
-		b.setFollowRedirect(true).setTcpNoDelay(true).setKeepAlive(true).setConnectTimeout(-1)
-				.setReadTimeout(requestTimeoutInSeconds == -1 ? requestTimeoutInSeconds : requestTimeoutInSeconds * 1000).setUserAgent(WASYNC_USER_AGENT);
-		return new DefaultAsyncHttpClient(b.build());
-	}
-=======
-    	NettyAsyncHttpProviderConfig nettyConfig = new NettyAsyncHttpProviderConfig();
-        nettyConfig.addProperty("child.tcpNoDelay", "true");
-        nettyConfig.addProperty("child.keepAlive", "true");
-        return createDefaultAsyncHttpClient(o.requestTimeoutInSeconds(), nettyConfig);  
-    }
-    
-    public final static AsyncHttpClient createDefaultAsyncHttpClient(Options o, AsyncHttpProviderConfig asyncHttpProviderConfig) {
-        return createDefaultAsyncHttpClient(o.requestTimeoutInSeconds(), asyncHttpProviderConfig);  
-    }
-    
-    public final static AsyncHttpClient createDefaultAsyncHttpClient(int requestTimeoutInSeconds, AsyncHttpProviderConfig asyncHttpProviderConfig) {
-        AsyncHttpClientConfig.Builder b = new AsyncHttpClientConfig.Builder();
-        b.setFollowRedirect(true).setConnectTimeout(-1)
+    public final static AsyncHttpClient createDefaultAsyncHttpClient(int requestTimeoutInSeconds) {
+        DefaultAsyncHttpClientConfig.Builder b = new DefaultAsyncHttpClientConfig.Builder();
+        b.setFollowRedirect(true).setTcpNoDelay(true).setKeepAlive(true).setConnectTimeout(-1)
                 .setReadTimeout(requestTimeoutInSeconds == -1 ? requestTimeoutInSeconds : requestTimeoutInSeconds * 1000)
                 .setUserAgent(WASYNC_USER_AGENT)
                 .setUseProxySelector(true);
-        AsyncHttpClientConfig config = b.setAsyncHttpClientProviderConfig(asyncHttpProviderConfig).build();
-        return new AsyncHttpClient(config);
+        return new DefaultAsyncHttpClient(b.build());
     }
->>>>>>> Use proxy selector with websockets
-       
+
     public static Socket create(Options options) {
         return create(options, DefaultSocket.class);
     }
