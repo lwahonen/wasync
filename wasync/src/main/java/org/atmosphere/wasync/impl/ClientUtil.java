04311e7caf33afeb2ba0e9af7e7a2a6b5815fb0a
Lauri Wiljami Ahonen <lwahonen@iki.fi>
2019-07-31 17:27:33 +0300

    Fix whitespace

--- wasync/src/main/java/org/atmosphere/wasync/impl/ClientUtil.java
+++ wasync/src/main/java/org/atmosphere/wasync/impl/ClientUtil.java
@@ -1,25 +1,25 @@
-        /*
-         * Copyright 2017 Async-IO.org
-         *
-         * Licensed under the Apache License, Version 2.0 (the "License"); you may not
-         * use this file except in compliance with the License. You may obtain a copy of
-         * the License at
-         *
-         * http://www.apache.org/licenses/LICENSE-2.0
-         *
-         * Unless required by applicable law or agreed to in writing, software
-         * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-         * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-         * License for the specific language governing permissions and limitations under
-         * the License.
-         */
-        package org.atmosphere.wasync.impl;
+/*
+ * Copyright 2017 Async-IO.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.atmosphere.wasync.impl;
 
-        import org.asynchttpclient.AsyncHttpClient;
-        import org.asynchttpclient.DefaultAsyncHttpClient;
-        import org.asynchttpclient.DefaultAsyncHttpClientConfig;
-        import org.atmosphere.wasync.Options;
-        import org.atmosphere.wasync.Socket;
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.DefaultAsyncHttpClient;
+import org.asynchttpclient.DefaultAsyncHttpClientConfig;
+import org.atmosphere.wasync.Options;
+import org.atmosphere.wasync.Socket;
 
 /**
  * Util class for building {@link AsyncHttpClient}
